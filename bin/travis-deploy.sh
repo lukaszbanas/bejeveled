7987f647f118f3e78cfc237897ae3dfe03f815d6
lukasz banas <kontakt@lukaszbanas.com.pl>
2018-10-19 10:46:15 +0200

    added travis jobs chain

--- bin/travis-deploy.sh
+++ bin/travis-deploy.sh
@@ -4,8 +4,9 @@ abort() {
     echo $message
     exit -1
 }
-[ -z $FTP_PASSWORD ] && abort "FTP_PASSWORD is undefined"
-[ -z $FTP_USER ] && abort "FTP_USER is undefined"
+[ -z ${FTP_PASSWORD} ] && abort "FTP_PASSWORD is undefined"
+[ -z ${FTP_USER} ] && abort "FTP_USER is undefined"
+[ -z ${FTP_ADDRESS} ] && abort "FTP_ADDRESS is undefined"
 
-lftp -u $FTP_USER,$FTP_PASSWORD ftp://s45.linuxpl.com/ \
+lftp -u ${FTP_USER},${FTP_PASSWORD} ${FTP_ADDRESS} \
  -e 'set ftp:ssl-allow no; mirror -c -e -R dist ~ ; exit'
